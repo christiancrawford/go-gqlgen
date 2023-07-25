5909ab41b545d1d23cffdc4764718f2b47ad485f
Christian Crawford <christian.crawford@pm.me>
2023-07-25 12:08:49 -0400

    add db connection params

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/99designs/gqlgen v0.17.35
+	github.com/joho/godotenv v1.5.1
 	github.com/vektah/gqlparser/v2 v2.5.7
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.2
--- graph/common/db.go
+++ graph/common/db.go
@@ -2,15 +2,36 @@
 package common
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/christiancrawford/go-gqlgen/graph/customTypes"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
+func goDotEnvVariable(key string) string {
+	// load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
 func InitDb() (*gorm.DB, error) {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+
+	dbconf := fmt.Sprintf("user=%s password=%s dbname=%s host=%s", dbUser, dbPassword, dbName, dbHost)
+
 	var err error
-	db, err := gorm.Open(postgres.Open("dev.db"), &gorm.Config{
+	db, err := gorm.Open(postgres.Open(dbconf), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
 	if err != nil {
